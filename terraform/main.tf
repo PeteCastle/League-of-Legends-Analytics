1361e771048dd839fec71d94f2a4b7b14019e540
PeteCastle <francis_mark_cayco@yahoo.com>
2023-04-07 23:38:18 +0800

    Added databricks and cluster in terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,34 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 2.0"
     }
+    databricks = {
+      source  = "databricks/databricks"
+      version = ">=1.14.2"
+    }
   }
+}
+
+provider "databricks" {
+  host = azurerm_databricks_workspace.db_project.workspace_url
+}
+
+data "databricks_current_user" "me"{
+  depends_on = [azurerm_databricks_workspace.db_project]
+}
+
+data "databricks_node_type" "smallest" {
+  local_disk = true
+  category = "General Purpose"
+}
+
+data "databricks_spark_version" "latest_version" {
+  latest = true
+  long_term_support = true
+  ml = false
+  gpu = false
+  scala = "2.12"
+  spark_version = "3.3.2"
+
 }
 
 locals{
@@ -35,4 +62,33 @@ resource "azurerm_storage_container" "sc_project" {
   name                  = "zoomcampcontainerproject"
   storage_account_name  = azurerm_storage_account.sa_project.name
   container_access_type = "blob"
+}
+
+resource "azurerm_databricks_workspace" "db_project" {
+  name                = "zoomcampdbproject"
+  resource_group_name = azurerm_resource_group.rg_project.name
+  location            = azurerm_resource_group.rg_project.location
+  sku                 = "standard"
+  managed_resource_group_name = "zoomcampdbproject-rg"
+}
+
+resource "databricks_cluster" "cluster_project" {
+  cluster_name            = "zoomcampclusterproject"
+  node_type_id            = data.databricks_node_type.smallest.id
+  spark_version           = data.databricks_spark_version.latest_version.id
+
+  autotermination_minutes = 10
+  num_workers = 0
+  # autoscale {
+  #   min_workers = 2
+  #   max_workers = 4
+  # }
+  spark_conf = {
+    "spark.databricks.cluster.profile" = "singleNode"
+    "spark.databricks.delta.preview.enabled" = "true"
+    "spark.master" : "local[*]"
+  }
+  custom_tags = {
+    "ResourceClass" = "SingleNode"
+  }
 }
